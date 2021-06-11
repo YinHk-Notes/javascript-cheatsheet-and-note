b5a8d697da20a6a36a16b07e1156262ace1c9032
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-11 17:52:05 +0800

    Update Array.js

--- script/array/Array.js
+++ script/array/Array.js
@@ -1,1 +1,36 @@
+// create an array
+let arr = []
+
+// assess an array item
+let value = arr[index]
+
+// add item to at the end
+arr.push(item)
+
+// remove item from the end
+arr.pop() 
+
+// remove item from beginning
+arr.shift()
+
+// remove item from beginning
+arr.unshift()
+
+// find index of specific item in array
+arr.indexOf(item)
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
