25c527f215f2d475d2ef437570547f2a6c38cf5b
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-15 17:33:40 +0800

    Update operator.js

--- script/operator/operator.js
+++ script/operator/operator.js
@@ -7,8 +7,9 @@ in
 // returns boolean to check if the specified object is an instance of the specified object.
 instanceof
   
-// check equal value and equal type
-=== 
+// equal value and equal type
+expr1 === expr2
 
-
+// 
+expr1 && expr2
 
