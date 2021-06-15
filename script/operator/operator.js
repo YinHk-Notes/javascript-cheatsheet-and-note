ff9ca0494eaf4c70e93629dc78797bd7a3a3d49a
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-15 17:18:33 +0800

    Update operator.js

--- script/operator/operator.js
+++ script/operator/operator.js
@@ -1,6 +1,12 @@
 // returns the type of a variable, object, function or expression
 typeof
 
+// return boolean to check if the specified property is in the specified object.
+in  
+  
+// returns boolean to check if the specified object is an instance of the specified object.
+instanceof
+  
 // check equal value and equal type
 === 
 
