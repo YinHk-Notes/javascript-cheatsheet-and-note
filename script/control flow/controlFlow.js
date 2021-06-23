78aa178d0bfd0d5955c1c4953f5763fdefdd6a2d
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-23 11:13:22 +0800

    Create controlFlow.js

---
+++ script/control flow/controlFlow.js
@@ -0,0 +1,31 @@
+//if statement
+if(condition){
+    // code
+}
+
+
+//if-else statement:
+if(condition){
+    // code
+} else if {
+    // code
+} else // code
+
+
+//switch statement:
+switch(condition){
+    case 'value1' :
+        //code
+        [break;]
+    case 'value2' :
+        //code
+        [continue;]
+    .......
+    default :
+        //code
+}
+
+
+  
+  
+  
