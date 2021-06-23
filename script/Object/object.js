d33a728a48a3571dd63a23130b5b655401318509
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-23 16:32:58 +0800

    Update object.js

--- script/Object/object.js
+++ script/Object/object.js
@@ -1,5 +1,28 @@
 
 
 
-//
-Object.keys()
+
+//create an array of a given object's own enumerable property names
+Object.keys(obj) //output: create an array  ["a", "b", "c"]
+/*
+// simple array
+const arr = ['a', 'b', 'c'];
+console.log(Object.keys(arr)); // console: ['0', '1', '2']
+
+// array-like object
+const obj = { 0: 'a', 1: 'b', 2: 'c' };
+console.log(Object.keys(obj)); // console: ['0', '1', '2']
+
+// array-like object with random key ordering
+const anObj = { 100: 'a', 2: 'b', 7: 'c' };
+console.log(Object.keys(anObj)); // console: ['2', '7', '100']
+
+// getFoo is a property which isn't enumerable
+const myObj = Object.create({}, {
+  getFoo: {
+    value: function () { return this.foo; }
+  }
+});
+myObj.foo = 1;
+console.log(Object.keys(myObj)); // console: ['foo']
+*/
