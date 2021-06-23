e38496cf95726606158bebdb4381b19740246e5a
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-23 14:31:25 +0800

    Create arrowFunction.js

---
+++ script/arrow function/arrowFunction.js
@@ -0,0 +1,6 @@
+functionName = (parameters)  => {...return }
+(parameters) => {...return }
+(parameters) => {... }    
+single parameter => {...  }
+( ) => { }  //no parameter
+( ) => return  //no parameter
