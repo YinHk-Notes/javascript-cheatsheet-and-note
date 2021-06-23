4817bf813cc3618016a0a6b2e489e1403928556a
Yin <67538811+YinHk@users.noreply.github.com>
2021-06-23 16:33:50 +0800

    Update object.js

--- script/Object/object.js
+++ script/Object/object.js
@@ -16,13 +16,4 @@ console.log(Object.keys(obj)); // console: ['0', '1', '2']
 // array-like object with random key ordering
 const anObj = { 100: 'a', 2: 'b', 7: 'c' };
 console.log(Object.keys(anObj)); // console: ['2', '7', '100']
-
-// getFoo is a property which isn't enumerable
-const myObj = Object.create({}, {
-  getFoo: {
-    value: function () { return this.foo; }
-  }
-});
-myObj.foo = 1;
-console.log(Object.keys(myObj)); // console: ['foo']
 */
