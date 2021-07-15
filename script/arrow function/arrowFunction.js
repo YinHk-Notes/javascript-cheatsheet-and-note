f64c83ff669f74942345ed606b1743546117d5c0
Yin <67538811+YinHk@users.noreply.github.com>
2021-07-15 10:52:11 +0800

    Update arrowFunction.js

--- script/arrow function/arrowFunction.js
+++ script/arrow function/arrowFunction.js
@@ -1,6 +1,11 @@
-functionName = (parameters)  => {...return }
-(parameters) => {...return }
-(parameters) => {... }    
-single parameter => {...  }
-( ) => { }  //no parameter
-( ) => return  //no parameter
+functionName = (parameters)  => { return... }
+(parameters) => { return... }
+(parameters) => ...expression    
+single parameter => ...expression  
+( ) => ...expression  //no parameter
+( ) => { return ... }  //no parameter
+
+
+//Be carefull Arrow Function Return
+//if an arrow function is simply returning a single line of code, you can omit the statement brackets and the return keyword. such as as (parameters) => ...expression.
+//if has return keyword, it must have bracket, (parameters) => { return... }
