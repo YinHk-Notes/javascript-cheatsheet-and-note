d1644948708c9872d3e414bc709478db11f83da8
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-14 17:09:51 +0800

    Create Function.js

---
+++ cheat sheet/Function/Function.js
@@ -0,0 +1,27 @@
+/* Every JavaScript function is actually a Function object.
+   This can be seen with the code (function(){}).constructor === Function, which returns true */
+
+//creates a new Function object, args and functionBody are all in string format
+const fun1 = new Function(functionBody)
+const fun2 = new Function(arg1, ... argN, functionBody)       
+
+//eg:
+const greet = new Function('console.log("hello world")');
+greet();
+
+(function(args1,args2,...,argsN) {}).length             //returns the number of formal parameters, excludes the 'rest parameter'.
+(function(...args) {}).name()                           //returns the function's name
+
+(function(...args) {}).apply(argsArray)                 //calls a function with a given this value, and arguments provided as an array (or an array-like object)
+(function(...args) {}).apply(thisArg, argsArray)
+
+const boundFun = fun.bind(this)                         //creates a new bound function that wraps the original function object. Calling the bound function generally results in the execution of its wrapped function
+const boundFun = fun.bind(this, arg1, ... , argN)      
+
+fun.call()                                              //allows for a function/method belonging to one object to be assigned and called for a different object
+fun.call(this)
+fun.call(this, arg1, ... , argN)
+
+fun.toString()                                          //returns a string representing the source code of the function
+
+
