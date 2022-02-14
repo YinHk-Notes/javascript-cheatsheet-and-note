4509d46e7ce61b03bed90f2277b52b9c50a9316b
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-14 14:34:30 +0800

    Update and rename math to math.js

---
+++ cheat sheet/Math/math.js
@@ -0,0 +1,20 @@
+Math.E                                //e=2.718....
+Math.LN2                              //Natural logarithm of 2; approximately 0.693...
+Math.LN10                             //Natural logarithm of 10; approximately 2.303...
+Math.PI                               //3.14...
+Math.exp(x)                           //returns e^x
+Math.expm1(x)                         //returns subtracting 1 from exp(x)
+Math.abs(x)                           //returns absolute value of x
+Math.ceil(x)                          //returns the smallest integer greater than or equal to x
+Math.floor(x)                         //returns the largest integer less than or equal to x
+Math.log(x)                           //returns the natural logarithm (㏒e; also, ㏑) of x
+Math.log10(x)                         //returns the base-10 logarithm of x
+Math.pow(base, exponent)              //base^exponent
+Math.random()                         //returns a random number between 0 and 1
+Math.round(x)                         //returns the value of the number x rounded to the nearest integer
+Math.trunc(x)                         //returns the integer portion of x, removing any fractional digits
+Math.sqrt(x)                          //returns the positive square root of x
+Math.max(val1, val2, ... , valN) 
+Math.min(val1, val2, ... , valN)
+
+
