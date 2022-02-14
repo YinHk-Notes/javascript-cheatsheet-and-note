d27e5beb4cf64ac96271f091f40f853f8b6783b9
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-14 12:20:08 +0800

    Update Set.js

--- cheat sheet/Set/Set.js
+++ cheat sheet/Set/Set.js
@@ -1,1 +1,8 @@
+/* Set objects are collections of values. You can iterate through the elements of a set in insertion order
+   It likes an array, but cannot has duplicated value. A value in the Set may only occur once; it is unique 
+   in the Set's collection. Set object lets you store unique values of any type, whether primitive values or object references */
+
+const set = new Set();              //creates a new Set object
+set.size;                           //returns the number of (unique) elements in a Set
+
 
