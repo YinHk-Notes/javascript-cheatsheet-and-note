4f5b1a7400ec313718ffeab434fbdc38e7f042de
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-14 14:02:15 +0800

    Update loop.js

--- cheat sheet/loop/loop.js
+++ cheat sheet/loop/loop.js
@@ -1,27 +1,60 @@
 //for loop
 for(Initialization; Condition; Increment/decrement){  
-//code  
+  //code  
 } 
 
 //eg:
 for(let i=0; i<=arr.length-1; i++){  
-//code  
+  //...  
 } 
+
+//for...of loop
+for(const element of array) {
+  //...
+}
+
+//for...in loop
+for(const property in object) {
+  //...
+}
+
+//for await...of loop
+/*a loop iterating over async iterable objects as well as on sync iterables, for await...of doesn't work 
+  with async iterators that are not async iterables. This statement can only be used inside an async function.*/
+for await (const variable of iterable) {
+  //...
+}
+
+//eg:
+async function* asyncGenerator(){
+  let i = 0;
+  while (i < 3) {
+    yield i++;
+  }
+}
+
+(async function(){
+  for await (let num of asyncGenerator()) {
+    console.log(num);
+  }
+})();
+// 0
+// 1
+// 2
+
 
 //while loop
 while (condition) {  
-  // code 
+  //code 
 }  
 
 //eg:
 while (true) {  
-  // code 
+  //...
 }  
 
 //do while loop
 do {  
-  // code 
+  //... 
 } while (condition);
-
-
 
