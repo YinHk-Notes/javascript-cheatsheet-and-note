bf87e79315ab6ef23cfebc11073902e3eb65e927
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-15 16:54:27 +0800

    Update module.js

--- cheat sheet/import & export/module.js
+++ cheat sheet/import & export/module.js
@@ -9,9 +9,75 @@ export class aClass {
 
 //import from multi export 
 import {aString, aObject, aArray, aFunction, aClass} from './location'
-import  * as myModule from './location'                                       //import 
+import  * as myModule from './location'                  //import all
 
 //single import
 export default moduleName
+import aFunction from './location'         //import from default export                                       
+
                             
+/* export syntax */
                             
+export var x = 42;                      // export a named variable
+export function foo() {};               // export a named function
+
+export default 42;                      // export the default export
+export default function foo() {};       // export the default export as a function
+
+export { encrypt };                     // export an existing variable
+export { decrypt as dec };              // export a variable as a new name
+export { encrypt as en } from 'crypto'; // export an export from another module
+export * from 'crypto';                 // export all exports from another module
+
+// Exporting individual features
+export let name1, name2, …, nameN; // also var, const
+export let name1 = …, name2 = …, …, nameN; // also var, const
+export function functionName(){...}
+export class ClassName {...}
+
+// Export list
+export { name1, name2, …, nameN };
+
+// Renaming exports
+export { variable1 as name1, variable2 as name2, …, nameN };
+
+// Exporting destructured assignments with renaming
+export const { name1, name2: bar } = o;
+
+// Default exports
+export default expression;
+export default function (…) { … }        // also class, function*
+export default function name1(…) { … }   // also class, function*
+export { name1 as default, … };
+
+// Aggregating modules
+export * from …; // does not set the default export
+export * as name1 from …; // Draft ECMAScript® 2O21
+export { name1, name2, …, nameN } from …;
+export { import1 as name1, import2 as name2, …, nameN } from …;
+export { default, … } from …;
+  
+
+/* import syntax */
+                               
+import 'jquery';                        // import a module without any import bindings
+import $ from 'jquery';                 // import the default export of a module
+import { $ } from 'jquery';             // import a named export of a module
+import { $ as jQuery } from 'jquery';   // import a named export to a different name
+import * as crypto from 'crypto';    // import an entire module instance object
+import ...from'@module-name'         //using @ instead of ./ or ../
+
+import defaultExport from "module-name";
+import * as name from "module-name";
+import { export1 } from "module-name";
+import { export1 as alias1 } from "module-name";
+import { export1 , export2 } from "module-name";
+import { foo , bar } from "module-name/path/to/specific/un-exported/file";
+import { export1 , export2 as alias2 , [...] } from "module-name";
+import defaultExport, { export1 [ , [...] ] } from "module-name";
+import defaultExport, * as name from "module-name";
+import "module-name";
+
+
+
+var promise = import("module-name");     
