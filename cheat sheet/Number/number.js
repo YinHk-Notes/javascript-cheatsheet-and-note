4ba817279905daa8106239efc88cb75f92334378
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-16 12:26:06 +0800

    Update number.js

--- cheat sheet/Number/number.js
+++ cheat sheet/Number/number.js
@@ -12,3 +12,26 @@ Number.MIN_VALUE              //represents the smallest positive numeric value r
 
 Number.NaN                    //Not-A-Number
 Number.NEGATIVE_INFINITY      //same as the negative value of the global object's Infinity property
+Number.POSITIVE_INFINITY      //same as the value of the global object's Infinity property.
+
+
+//methods
+Number.isFinite(value)                    //determines whether the passed value is a finite number 
+Number.isInteger(value)                   //determines whether the passed value is an integer
+Number.isNaN(value)                       //determines whether the passed value is NaN and its type is Number
+Number.isSafeInteger(testValue)           //determines whether the provided value is a number that is a safe integer
+Number.parseFloat(string)                 //parses an argument and returns a floating point number
+Number.parseInt(string)
+Number.parseInt(string, radix)            //parses a string argument and returns an integer of the specified radix or base
+Number.toExponential()                    //returns a string representing the Number object in exponential notation
+Number.toExponential(fractionDigits)  
+Number.toFixed(digits)                    //formats a number using fixed-point notation
+Number.toLocaleString(locales, options)   //returns a string with a language-sensitive representation of this number
+Number.toPrecision(precision)             //returns a string representing the Number object to the specified precision, precision Optional: an integer specifying the number of significant digits
+
+num.toString()                            //returns a string representing the specified Number object
+num.valueOf()                             //returns the wrapped primitive value of a Number object
+
+
+
+
