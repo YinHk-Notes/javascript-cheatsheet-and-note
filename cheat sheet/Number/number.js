afa04747dfccf7058567f67119d83ce53b4e5729
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-16 10:46:40 +0800

    Update number.js

--- cheat sheet/Number/number.js
+++ cheat sheet/Number/number.js
@@ -1,1 +1,14 @@
+/* Number is a primitive wrapper object used to represent and manipulate numbers */
 
+//create a number object
+const num = new Number(value)
+
+
+Number.MAX_SAFE_INTEGER       //2^53 - 1
+Number.MAX_VALUE              //represents the maximum numeric value representable in JavaScript
+
+Number.MIN_SAFE_INTEGER       //-(2^53 - 1)
+Number.MIN_VALUE              //represents the smallest positive numeric value representable in JavaScript
+
+Number.NaN                    //Not-A-Number
+Number.NEGATIVE_INFINITY      //same as the negative value of the global object's Infinity property
