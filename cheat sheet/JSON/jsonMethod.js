f2a913c2434c2ba73a5ddf8c06fd42af5ed8f010
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-17 16:42:33 +0800

    Update jsonMethod.js

--- cheat sheet/JSON/jsonMethod.js
+++ cheat sheet/JSON/jsonMethod.js
@@ -1,1 +1,9 @@
+//Parses a JSON string
+const json = '{"id": 1001, "user_name": "Yamada", "age": 18}';
+const obj = JSON.parse(json);                                   //{ id: 1001, 'user_name': 'Yamada', age: 18 }
+
+//Converts a JavaScript object or value to a JSON string
+const obj2 = {id: 1002, user name: "Kimura", age: 23}
+const toJson = JSON.stringify(obj2);                            //'{"id":1002,"user_name":"Kimura","age":23}'
+
 
