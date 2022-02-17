20260679de513906bd05950fead459d8276de652
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-17 17:49:38 +0800

    Update symbol.js

--- cheat sheet/Symbol/symbol.js
+++ cheat sheet/Symbol/symbol.js
@@ -1,1 +1,6 @@
+/* Symbol is a built-in object whose constructor returns a symbol primitive — also called a Symbol value 
+   or just a Symbol — that’s guaranteed to be unique. */
 
+//create a symbol object
+let sym = Symbol()
+let sym2 = Symbol(description)
