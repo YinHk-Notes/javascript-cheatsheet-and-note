d6b21c490e1bc722e1c200aca30c002c2ac92349
Yin <67538811+YinHk@users.noreply.github.com>
2022-02-27 01:14:53 +0800

    Update protoTypeChain.js

--- cheat sheet/proto type chain/protoTypeChain.js
+++ cheat sheet/proto type chain/protoTypeChain.js
@@ -1,18 +1,39 @@
-/* Prototypes are the mechanism by which JavaScript objects inherit features from one another.
-   All JavaScript objects inherit properties and methods from a prototype, all browsers use "__proto__" to represent proto type */
+/* JavaScript is prototype-base-oriented language, there is no class inheritance in JavaScript.
+   Prototypes are the mechanism by which JavaScript objects inherit features from one another.
+   All JavaScript objects inherit properties and methods from its prototype. */
+
+
+// [[Prototype]] 
+// This is a special hidden property that exists on every object in JavaScript, it holds a reference to the object’s prototype.
+// The value of this property is always either null or name of another object.
+// [[Prototype]] is null it means that the object doesn’t inherit from any other object.
+// The value is a name of another object it means that the object’s prototype references another object.
+// You only read it from this hidden property, and not allow to modify the value from this property.
+
+
+// __proto__ 
+// It is a default property added to every object. This property points to the prototype of an object.
+// It is a method that exposes the hidden [[Prototype]] property and allows you to modify it.
+// It is not the same thing to [[Prototype]], __proto__ is a setter and getter for [[Prototype]] property.
+// You can use them to change the prototype of an object.
+// .__proto__ must point to another prototype of object
+
+// get the prototype of object
+let obj = {}
+obj.__proto__              //Object.prototype
+obj.__proto__.__proto__    //null
+
+// set prototype to another object for inheritance 
+const baseObj = {}
+const newObj = {}
+newObj.__proto__ = baseObj  //newObj inherit from baseObj, by setting newObj prototype to baseObj
+
+// Note: Object.prototype.__proto__ is deprecated and not a standard.
+// Try to use the following methods for get and set instead
+Object.getPrototypeOf(obj)                //get method
+Object.setPrototypeOf(obj, prototype)     //set method
+
 
 /* using prototype to add new feature:
    - add new properties (or methods) to all existing objects of a given type
    - add new properties (or methods) to an object constructor */
-
-//eg:
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-
-
-
-//get prototype
-const 
