5cf03a62499b077114f8cace4f446d500be942c1
Yin <67538811+YinHk@users.noreply.github.com>
2022-04-17 10:59:32 +0800

    Create High-order-function.js

---
+++ cheat sheet/Functions/High-order-function.js
@@ -0,0 +1,33 @@
+//HOF
+
+//A Higher-Order Function is a regular function that takes one or more functions as arguments and/or returns 
+//a function as a value from it.
+
+//eg:
+// Define a function that takes a function as an argument.
+function getCapture(camera) {
+  // Invoke the passed function
+  camera();
+}
+
+// Invoke the function by passing a function as an argument
+getCapture(function(){
+  console.log('Canon');
+});
+
+// Define a function that returns a function
+function returnFunc() {
+  return function() {
+    console.log('Hi');
+  }
+}
+
+// Take the returned function in a variable.
+const fn = returnFunc();
+// Now invoke the returned function.
+fn(); // logs 'Hi' in the console
+
+// Alternatively - A bit odd syntax but good to know
+returnFunc()(); // logs 'Hi' in the console
+
+
