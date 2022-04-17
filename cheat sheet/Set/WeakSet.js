a7a3831bd49e1f30c03b07f88d0d0c237727fae9
Yin <67538811+YinHk@users.noreply.github.com>
2022-04-17 10:17:56 +0800

    Update WeakSet.js

--- cheat sheet/Set/WeakSet.js
+++ cheat sheet/Set/WeakSet.js
@@ -1,1 +1,23 @@
+// The WeakSet object lets you store weakly held objects in a collection
+// WeakSet objects are collections of objects. Just as with Sets, each object in a WeakSet may occur 
+// only once; all objects in a WeakSet's collection are unique
 
+/* differences between the Set and weakset object are:
+    - WeakSets are collections of objects only. They cannot contain arbitrary values of any type, as Sets can
+    - The WeakSet is weak, meaning references to objects in a WeakSet are held weakly. If no other references to an object 
+      stored in the WeakSet exist, those objects can be garbage collected
+  */
+
+const ws = new WeakSet();
+const foo = {};
+const bar = {};
+
+ws.add(foo);
+ws.add(bar);
+
+ws.has(foo);    // true
+ws.has(bar);    // true
+
+ws.delete(foo); // removes foo from the set
+ws.has(foo);    // false, foo has been removed
+ws.has(bar);    // true, bar is retained
