d559e7cfb895c4d04955ee069c2e13d196e79132
Yin <67538811+YinHk@users.noreply.github.com>
2022-04-17 10:25:29 +0800

    Create truthy & falsy.js

---
+++ cheat sheet/Boolean/truthy & falsy.js
@@ -0,0 +1,45 @@
+//Falsy
+//falsy values in JavaScript:
+if (false)
+if (null)
+if (undefined)
+if (0)
+if (-0)
+if (0n)
+if (NaN)
+if ("")
+
+//The logical AND operator, &&
+// If the first object is falsy, it returns that object
+  
+false && "dog"
+// ↪ false
+
+0 && "dog"
+// ↪ 0
+
+
+//Truthy
+//truthy values in JavaScript 
+if (true)
+if ({})
+if ([])
+if (42)
+if ("0")
+if ("false")
+if (new Date())
+if (-42)
+if (12n)
+if (3.14)
+if (-3.14)
+if (Infinity)
+if (-Infinity)
+  
+//The logical AND operator, &&
+true && "dog"
+// returns "dog"
+
+[] && "dog"
+// returns "dog"
+  
+  
