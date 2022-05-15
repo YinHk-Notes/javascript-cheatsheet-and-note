ac3efcaea735faabb6d64d962c8185e8b955b497
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-15 01:28:23 -0400

    Update promise.js

--- cheat sheet/Promise/promise.js
+++ cheat sheet/Promise/promise.js
@@ -105,6 +105,9 @@ myPromise.then((value) => { return new Promise( (resolve, reject) => {}) })
    
    
 //takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises   
+//This returned promise will resolve when all of the input's promises have resolved, 
+//or if the input iterable contains no promises.
+// It rejects immediately upon any of the input promises rejecting or non-promises throwing an error
 Promise.all([promise1, promise2, promise3,...promise])
 
 //example:
