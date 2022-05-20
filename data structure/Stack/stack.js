b10c2929f1a8b6f7d3a02e519f88cbf30e2c7fe0
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-20 14:17:23 -0400

    Update stack.js

--- data structure/Stack/stack.js
+++ data structure/Stack/stack.js
@@ -1,1 +1,42 @@
+class Stack { 
+  constructor() {
+    this.dataStore = [];
+    this.top = null;
+    this.size = 0;
+  }
+  
+  // add the value to the top
+  push(value) {
+    this.dataStore.push(value);
+    this.top = value;
+    this.size++;
+  }
+  
+  // remove the top
+  pop() {
+    if(size > 0) {
+      this.size--; 
+      this.top = this.dataStore[size-1];
+      return this.dataStore.pop();
+    } else return null;
+  }
+  
+  // returns the top element of the stack
+  peek() {
+    return this.top;
+  }
+  
+  // check is empty stack
+  isEmpty() {
+    if(size = 0)
+      return true;
+    else return false;
+  }
+  
+  // display stack
+  stack() {
+    return this.dataStore;
+  }
+
+}
 
