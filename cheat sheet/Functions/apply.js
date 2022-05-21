c89de3ed9599fa7c82576ff1f7f008b63a4d3b71
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-21 00:12:20 -0400

    Create apply.js

---
+++ cheat sheet/Functions/apply.js
@@ -0,0 +1,17 @@
+// With the apply() method, you can write a method that can be used on different objects.
+
+// The apply() method takes arguments as an array.
+
+const person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+person.fullName.call(person1, "Oslo", "Norway");
+
