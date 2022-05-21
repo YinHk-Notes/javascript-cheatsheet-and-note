ede6b1a782dbe7e4ed13b2b68878836e8aec0b78
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-20 20:46:37 -0400

    Update array.js

--- cheat sheet/Array/array.js
+++ cheat sheet/Array/array.js
@@ -170,7 +170,7 @@ arr.join(separator) //optional, default separator is comma (,), specified separa
 /* 
    ed: arr=["a","b","c"] 
    arr.join() //output: "a,b,c"
-   arr.join('') //output: "abc"
+   arr.join('') //output: "abc", ---> this method turns items in array into single string
    arr.join('/') //output: "a/b/c"
    arr.join('+') //output: "a+b+c"
  */
