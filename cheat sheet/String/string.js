2205b18942847f7fdc4ed1fe9a2ecad3648d1316
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-20 21:57:00 -0400

    Update string.js

--- cheat sheet/String/string.js
+++ cheat sheet/String/string.js
@@ -44,13 +44,13 @@ str.search(regexp)                              //returns index of the first mat
 str.slice(beginIndex)                           //returns new string containing the extracted section of the string
 str.slice(beginIndex, endIndex)                 //If start == stop, returns an empty string
 
+str.substring(indexStart)                       //returns the part of the string between the start and end indexes, or to the end of the string
+str.substring(indexStart, indexEnd)             //If start == stop, returns an empty string
+
 str.split(separator)                            //divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array
 str.split(separator, limit)                     //The pattern describing where each split should occur. The separator can be a simple string or it can be a regular expression.
                                                 //If separator is an empty string (""), str is converted to an array of each of its characters. ---> string to array
 
-
-str.substring(indexStart)                       //returns the part of the string between the start and end indexes, or to the end of the string
-str.substring(indexStart, indexEnd)             //If start == stop, returns an empty string
 
 str.toLowerCase()
 str.toUpperCase()
