0f8a19ad6caacb3f874c9c3c8fa648b3c951627c
Ken <67538811+YinHk@users.noreply.github.com>
2022-07-03 12:54:00 -0400

    Update operator.js

--- cheat sheet/expression & operator/operator.js
+++ cheat sheet/expression & operator/operator.js
@@ -15,6 +15,14 @@ expr1 && expr2
 
 some_condition && some_action();  //if some_condition is not null/fullfil, then call the function
 
+return expr1 && expr2   //return expr1 if false else expr2
+
+// Logical OR
+expr1 || expr2    //if exp1 false, check for exp2
+
+return exp1 || exp2;  //return expr1 if true else expr2
+
+
 //Optional chaining (?.)
 obj.val?.prop
 obj.val?.[expr]
