1b1a8a55807ccb4779d1588b853127e3cfb1b3aa
Ken <67538811+YinHk@users.noreply.github.com>
2022-08-07 23:45:41 -0400

    Update High-order-function.js

--- cheat sheet/Functions/High-order-function.js
+++ cheat sheet/Functions/High-order-function.js
@@ -3,6 +3,13 @@
 //A Higher-Order Function is a regular function that takes one or more functions as arguments and/or returns a function as a value from it.
 //A Higher-Order function is a function that receives a function as an argument or returns the function as output.
 
+/*  
+    In mathematics and computer science, a higher-order function (HOF) is a function that does at least one of the following:
+    
+      1. takes one or more functions as arguments (i.e. a procedural parameter, which is a parameter of a procedure that is itself a procedure),
+      2. returns a function as its result.
+*/
+
 //eg:
 // Define a function that takes a function as an argument.
 function getCapture(camera) {
