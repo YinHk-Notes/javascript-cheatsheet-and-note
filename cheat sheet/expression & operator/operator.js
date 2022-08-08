6e2fa1267ee2d17372b0ce6fff942b2659f1374b
Ken <67538811+YinHk@users.noreply.github.com>
2022-08-08 17:01:45 -0400

    Update operator.js

--- cheat sheet/expression & operator/operator.js
+++ cheat sheet/expression & operator/operator.js
@@ -11,16 +11,16 @@ instanceof
 expr1 === expr2
 
 // Logical AND
-expr1 && expr2
+expr1 && expr2                    //if expr1 true, check for expr2
 
 some_condition && some_action();  //if some_condition is not null/fullfil, then call the function
 
-return expr1 && expr2   //return expr1 if false else expr2
+return expr1 && expr2             // if expr1 true return exp2, else just return expr1
 
 // Logical OR
-expr1 || expr2    //if exp1 false, check for exp2
+expr1 || expr2                    // if expr1 false, check for expr2
 
-return exp1 || exp2;  //return expr1 if true else expr2
+return exp1 || exp2;              // if expr1 false return expr2, else just return expr1
 
 
 //Optional chaining (?.)
